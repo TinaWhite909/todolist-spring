2213f472ed30f37380ef59603cc774426a3b553e
TinaWhite909 <69662806+TinaWhite909@users.noreply.github.com>
2021-06-14 13:29:48 +0300

    add statuses into DB

---
+++ src/main/resources/db/V1_2__table_stat.sql
@@ -0,0 +1,6 @@
+CREATE TABLE public.tableStatuses
+(
+    "STATUS_ID" integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "STATUS" character varying(1000) COLLATE pg_catalog."default"
+
+)
